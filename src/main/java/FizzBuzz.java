e7ea9097d3b775d0cf4c6e8af41ce72481c99cd0
Patricia Rosembluth <patty_rosembluth@hotmail.com>
2013-09-25 20:28:56 -0400

    codigo refactorizado

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,16 +1,27 @@
 
 public class FizzBuzz 
 {
-     
+	private static final int _Fizz=3;
+	private static final int _Buzz=5;
+	private static final String _MensajeFizz="Fizz";
+	private static final String _MensajeBuzz="Buzz";
+	private static final String _MensajeFizzBuzz="FizzBuzz";
 	public String Generar(int numero) 
 	{
-		if(numero%3==0 && numero%5==0)
-			return "FizzBuzz";
-		if (numero%3==0)
-			return "Fizz";
-		if(numero%5==0)
-			return "Buzz";
+		if(EsMultiplo(numero,_Fizz) && EsMultiplo(numero,_Buzz) )
+			return _MensajeFizzBuzz;
+		if (EsMultiplo(numero, _Fizz))
+			return _MensajeFizz;
+		if(EsMultiplo(numero, _Buzz))
+			return _MensajeBuzz;
 		return Integer.toString(numero);
 	}
 
+	public boolean EsMultiplo(int numero,int multiplo)
+	{
+		if(numero % multiplo == 0)
+			return true;
+		else
+			return false;
+	}
 }
